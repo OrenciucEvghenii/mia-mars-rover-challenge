b694017a9deca03824a602d6d0c2005ff4039dd4
Evghenii Orenciuc <evghenii.orenciuc@bitingbit.com>
2022-01-10 12:28:55 +0100

    rename test class to PlateauTest + optimize test methods

--- src/test/java/PlateauTest.java
+++ src/test/java/PlateauTest.java
@@ -1,48 +1,37 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import model.Plateau;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class MarsRoverTest {
-
-    @BeforeEach
-    void setUp() {
-    }
-
+class PlateauTest {
     @Test
     void setNegativeXForPlateauSize_illegalArgumentExceptionExpected() {
-        Exception exception = assertThrows(RuntimeException.class, () -> {
-            new Plateau(-1, 0);
-        });
+        assertThrows(RuntimeException.class, () -> new Plateau().setX(-1));
     }
 
     @Test
     void setNegativeYForPlateauSize_illegalArgumentExceptionExpected() {
-        Exception exception = assertThrows(RuntimeException.class, () -> {
-            new Plateau(0, -1);
-        });
+        assertThrows(RuntimeException.class, () -> new Plateau().setY(-1));
     }
 
     @Test
     void createZeroSizePlateau_validPlateauObjectExpected() {
-        Plateau plateau = new Plateau(0, 0);
+        Plateau plateau = new Plateau();
+        plateau.setX(0);
+        plateau.setY(0);
         assertThat(plateau.getX(), equalTo(0));
         assertThat(plateau.getY(), equalTo(0));
     }
 
     @Test
     void createPositiveSizePlateau_validPlateauObjectExpected() {
-        Plateau plateau = new Plateau(1, 2);
+        Plateau plateau = new Plateau();
+        plateau.setX(1);
+        plateau.setY(2);
         assertThat(plateau.getX(), equalTo(1));
         assertThat(plateau.getY(), equalTo(2));
-    }
-
-    @AfterEach
-    void tearDown() {
     }
 }
